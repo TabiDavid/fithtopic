c3f024f1b159167ad9706e9faca10fa8cf4fcf0e
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2019-11-09 21:32:23 -0500

    commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function saveLocalstorage(){
+
+    let person = {
+        name: "David",
+        age: 33,
+        email: "tabidavid@mailaxy.com",
+        country: "Ecuador"
+
+    }
+};
+
+let name = "Jhon";
+
+localStorage.setItem("name", name);
+localStorage.setItem("person", JSON.stringify(person));
+
+function obtainLocalstorage(){
+let name = localStorage.getItem("name");
+let person = localStorage.getItem("person");
+
+}
